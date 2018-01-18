6e48902e3465111d0a6d941583f4cf60cc025b59
Iain Foulds <iain.foulds@microsoft.com>
2018-01-18 12:48:38 -0800

    Create prepare_vm_disks.ps1

---
+++ prepare_vm_disks.ps1
@@ -0,0 +1,14 @@
+$disks = Get-Disk | Where partitionstyle -eq 'raw' | sort number
+
+$letters = 70..89 | ForEach-Object { [char]$_ }
+$count = 0
+$label = "datadisk"
+
+foreach ($disk in $disks) {
+    $driveLetter = $letters[$count].ToString()
+    $disk | 
+    Initialize-Disk -PartitionStyle MBR -PassThru |
+    New-Partition -UseMaximumSize -DriveLetter $driveLetter |
+    Format-Volume -FileSystem NTFS -NewFileSystemLabel $label.$count -Confirm:$false -Force
+$count++
+}
