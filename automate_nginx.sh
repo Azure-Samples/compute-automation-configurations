a43faf89a9a310b8e48fce55a4f1257267060ff2
Micah McKittrick <32313503+mimckitt@users.noreply.github.com>
2024-06-17 09:33:38 -0700

    Update automate_nginx.sh

--- automate_nginx.sh
+++ automate_nginx.sh
@@ -24,4 +24,4 @@
 
 apt-get update -y && apt-get upgrade -y
 apt-get install -y nginx && apt-get install -y dialog
-echo "Hello World from host" $HOSTNAME "!" | sudo tee -a /var/www/html/index.html
+echo "Hello World from host" $HOSTNAME "!" | sudo tee /var/www/html/index.html
